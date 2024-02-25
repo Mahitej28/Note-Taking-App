c1d9c42ec6f69179863e3c3902af2754c3d1619a
Mahima Churi <mahimachuri.28@gmail.com>
2024-02-25 18:55:16 +0530

    [UPDATED] Debugged Refreshing issue

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ def index():
     if request.method == "POST":
          note = request.form.get("note")
          notes.append(note)
+         return redirect(url_for('index'))
     indexed_notes = list(enumerate(notes))
     return render_template("home.html", notes= indexed_notes)
 
