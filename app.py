f01dee9a210436d756554716b5039228b0f16b63
Mahima Churi <mahimachuri.28@gmail.com>
2024-02-25 15:56:06 +0530

    [ADDED] Added Edit and Delete feature

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -7,9 +7,26 @@ notes = []
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        note = request.form.get("note")
-        notes.append(note)
-    return render_template("home.html", notes=notes)
+         note = request.form.get("note")
+         notes.append(note)
+    indexed_notes = list(enumerate(notes))
+    return render_template("home.html", notes= indexed_notes)
+
+@app.route('/edit', methods=["GET","POST"])
+def edit_note():
+    if request.method == "POST":
+        note_index = int(request.form.get("note_index"))
+        new_note = request.form.get("new_note")
+        notes[note_index] = new_note
+    return redirect(url_for('index'))
+
+@app.route('/delete', methods=["GET","POST"])
+def delete_note():
+    if request.method == "POST":
+        note_index = int(request.form.get("note_index"))
+        del notes[note_index]
+    return redirect(url_for('index'))
+ 
 
 
 
