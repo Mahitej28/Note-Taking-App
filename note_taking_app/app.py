eac9614aedee2b5f33e878712de4cc902feec832
Mahima Churi <mahimachuri.28@gmail.com>
2024-02-25 15:06:11 +0530

    [DEBUG] Code Refactored and Debugged

--- note_taking_app/app.py
+++ note_taking_app/app.py
@@ -3,14 +3,11 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 notes = []
-
-@app.route('/', methods=["GET", "POST"])
+@app.route('/', methods=["POST"])
 def index():
-    if request.method == "POST":
-        note = request.form.get("note")
-        notes.append(note)
+    note = request.args.get("note")
+    notes.append(note)
     return render_template("home.html", notes=notes)
-
 
 
 if __name__ == '__main__':
